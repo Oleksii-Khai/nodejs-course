283358be1f265e3c7824860a340f61d199ab795f
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2023-12-13 16:08:03 +0200

    add route-id/brand handler, query-params handler for 'products?'

--- product.routes.js
+++ product.routes.js
@@ -5,14 +5,29 @@ const { blockSpecialBrand } = require('./middleware');
 const router = express.Router();
 
 router.get('/products', async (req, res) => {
-  return res.json(products);
+  const id = req.query.id;
+  if (id && !isNaN(id)) {
+    const filteredProducts = products.filter(
+      (product) => product.id === Number(id)
+    );
+    return res.json(filteredProducts);
+  } else {
+    return res.json(products);
+  }
 });
 
-router.get('/products/:brand', blockSpecialBrand, (req, res) => {
-  const { brand } = req.params;
-  const filteredProducts = products.filter(
-    (product) => product.brand === brand
-  );
+router.get('/products/:param', blockSpecialBrand, (req, res) => {
+  const { param } = req.params;
+  let filteredProducts;
+  console.log(Number(param));
+  if (!isNaN(param)) {
+    filteredProducts = products.filter((product) => {
+      return product.id === Number(param);
+    });
+    console.log(filteredProducts);
+  } else if (param && isNaN(param)) {
+    filteredProducts = products.filter((product) => product.brand === param);
+  }
 
   res.json(filteredProducts);
 });
