e19eab42dbd3a14acf6fd1722f3e22239718c2cc
Oleksii-Khai <s.s.oleksiy@student.khai.edu>
2024-04-04 14:32:40 +0300

    feat(products): add route-handler to get user's products

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
+const { eq } = require('drizzle-orm');
 const db = require('../db');
-const { users } = require('../db/schema');
+const { users, products } = require('../db/schema');
 
 router.post('/users', async (request, response) => {
   const { body } = request;
@@ -12,6 +13,20 @@ router.post('/users', async (request, response) => {
 router.get('/users', async (request, response) => {
   const users = await db.query.users.findMany();
   return response.status(200).json({ users });
+});
+
+router.get('/users/:id/products', async (request, response) => {
+  const { id } = request.params;
+
+  //   next condition operator checks if user id in params is valid
+  if (id === ' ' || isNaN(id)) {
+    return response.sendStatus(400);
+  }
+
+  const userProducts = await db.query.products.findMany({
+    where: eq(products.userId, +id)
+  });
+  return response.status(200).json({ userProducts });
 });
 
 module.exports = router;
